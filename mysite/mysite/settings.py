a1c52218a13822ee9ddd53703a65cc8d209af696
Semyon Mikhailov <mikhailovsv@evrone.com>
2024-05-02 14:09:19 +0300

    Edit allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'igbx(6^k(3jsykj+ar&xs@8tsj@+8j#^5gm#86&5ja!f1(z@@5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["127.0.0.1:8000", "localhost:8000"]
 
 
 # Application definition
