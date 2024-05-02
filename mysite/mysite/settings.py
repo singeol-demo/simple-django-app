89247516e3eed5e221a815aab7b74fa355b09b9b
Semyon Mikhailov <mikhailovsv@evrone.com>
2024-05-02 13:52:01 +0300

    Disable debug

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'igbx(6^k(3jsykj+ar&xs@8tsj@+8j#^5gm#86&5ja!f1(z@@5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
